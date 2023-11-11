486b851b333db05adb60f788752d7f557112d014
mehrdad <jafari.ceai@gmail.com>
2023-11-11 12:48:42 +0330

    refactor strconv

--- 03/strconv/main.go
+++ 03/strconv/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
 
+const (
+	feetInMeters = 0.3048
+	feetInYards  = feetInMeters / 0.9144
+)
+
 func main() {
 	feet, _ := strconv.ParseFloat(os.Args[1], 64)
-	meter := feet * 0.3048
-	fmt.Printf("%f_feet is %f meters.\n", feet, meter)
-	fmt.Printf("%g_feet is %g meters.\n", feet, meter)
+	meter := math.Round(feet * feetInMeters)
+	yards := math.Round(feet * feetInYards)
+	fmt.Printf("%g feet is %g meters.\n", feet, meter)
+	fmt.Printf("%g feet is %g yards.\n", feet, yards)
 }
